4e54e0578b5755130aebbad7c3c4343f7a35404e
Junho Yu <95111999+ryuwon2407@users.noreply.github.com>
2024-05-20 03:17:50 +0900

    Feat: Font 추가 - Pretendard 폰트(9종)

--- Familiar/Util/Extension/Extension.swift
+++ Familiar/Util/Extension/Extension.swift
@@ -5,4 +5,37 @@
 //  Created by 준호 on 5/17/24.
 //
 
-import Foundation
+import SwiftUI
+
+// MARK: - UIFont
+extension UIFont {
+    static func pretendard(size fontSize: CGFloat, weight: UIFont.Weight) -> UIFont {
+        let familyName = "Pretendard"
+
+        var weightString: String
+        switch weight {
+        case .black:
+            weightString = "Black"
+        case .bold:
+            weightString = "Blod"
+        case .heavy:
+            weightString = "ExtraBold"
+        case .ultraLight:
+            weightString = "ExtraLight"
+        case .light:
+            weightString = "Light"
+        case .medium:
+            weightString = "Medium"
+        case .regular:
+            weightString = "Regular"
+        case .semibold:
+            weightString = "SemiBold"
+        case .thin:
+            weightString = "Thin"
+        default:
+            weightString = "Regular"
+        }
+
+        return UIFont(name: "\(familyName)-\(weightString)", size: fontSize) ?? .systemFont(ofSize: fontSize, weight: weight)
+    }
+}
